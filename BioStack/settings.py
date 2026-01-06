5b18640e4c600ac38f1ce6ab38122a746574f4e5
raju-07 <rajuyadav782760@gmail.com>
2026-01-06 11:04:44 +0530

    securing env's

--- BioStack/settings.py
+++ BioStack/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0&4ipusu4b2#l_%^ddhy)bav9_3i0x#ayw===6#hn41spolu77'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'BioStack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
 DATABASES = {
