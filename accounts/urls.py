5d83f429ea03daff6e99481420c0054f5f3e59ad
raju-07 <rajuyadav782760@gmail.com>
2026-01-07 22:01:36 +0530

    Adding left files to commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import signup_view,login_view,logout_view
 
-app_name = "accoutns"
+app_name = "accounts"
 
 urlpatterns = [
      # auth routing link 
--- accounts/views.py
+++ accounts/views.py
@@ -7,31 +7,31 @@ from .forms import SignupForm, LoginForm
 
 def signup_view(request):
     if request.user.is_authenticated:
-        return redirect("dashboard")
+        return redirect("dashboard:index")
 
     form = SignupForm(request.POST or None)
 
     if request.method == "POST" and form.is_valid():
         user = form.save()
         login(request, user)
-        return redirect("dashboard")
+        return redirect("dashboard:index")
 
     return render(request, "accounts/signup.html", {"form": form})
 
 
 def login_view(request):
     if request.user.is_authenticated:
-        return redirect("dashboard")
+        return redirect("dashboard:index")
 
     form = LoginForm(request.POST or None)
 
     if request.method == "POST" and form.is_valid():
         login(request, form.cleaned_data["user"])
-        return redirect("dashboard")
+        return redirect("dashboard:index")
 
     return render(request, "accounts/login.html", {"form": form})
 
 
 def logout_view(request):
     logout(request)
-    return redirect("login")
+    return redirect("accounts:login")
