8ba54f02db75783e9604ed743041f4073a407ba1
raju-07 <rajuyadav782760@gmail.com>
2026-01-08 12:51:59 +0530

    fixing duplicate namespace

--- BioStack/urls.py
+++ BioStack/urls.py
@@ -22,18 +22,10 @@ from django.conf.urls.static import static
 from BioStack import views
 from django.contrib.auth import views as auth_views
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#      path("auth/", include(("accounts.urls", "accounts"), namespace="accounts")), #Accounts app urls
-#     path("dashboard/", include(("dashboard.urls", "dashboard"), namespace="dashboard")), # Dashboard app urls
-    
-#     ]
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',views.homepage),
-    path("auth/", include(("accounts.urls", "accounts"), namespace="accounts")),
-    path("dashboard/", include(("dashboard.urls", "dashboard"), namespace="dashboard")),
+    
  # Namespaced apps
     path("auth/", include(("accounts.urls", "accounts"), namespace="accounts")),
     path("dashboard/", include(("dashboard.urls", "dashboard"), namespace="dashboard")),
