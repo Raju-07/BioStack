fcd920b1842179ff3fa9cd196a0026a6cd8cc11a
raju-07 <rajuyadav782760@gmail.com>
2026-01-23 21:45:27 +0530

    updated settings.py

--- BioStack/settings.py
+++ BioStack/settings.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -8,9 +7,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
-
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
-
 
 # Application definition
 
