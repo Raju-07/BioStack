75d3e58f895e1f0853b81ee310ce825e0c794886
raju-07 <rajuyadav782760@gmail.com>
2026-01-24 20:40:37 +0530

    update settings.py

--- BioStack/settings.py
+++ BioStack/settings.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -7,10 +6,8 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
-
 
 # Application definition
 
@@ -30,17 +27,18 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 #staticfiles 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'BioStack.urls'
 
@@ -83,9 +81,9 @@ DATABASES = {
 # S3 / Supabase Storage Settings
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'media'  # The name of the bucket you created
+AWS_STORAGE_BUCKET_NAME = 'media'
 AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
-AWS_S3_REGION_NAME = 'ap-south-1' # Or your specific region
+AWS_S3_REGION_NAME = 'ap-south-1' 
 AWS_DEFAULT_ACL = 'public-read'
 
 STORAGES = {
@@ -107,7 +105,6 @@ RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_USER_MODEL = "accounts.User"
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -133,17 +130,20 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 # Link for Static Files
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR,'static'
+]
 
 # Link for media
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
 TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates"]
 
 LOGIN_URL = "/auth/login/"
@@ -155,7 +155,7 @@ DEFAULT_FROM_EMAIL = "noreply@biostack.local"
 
 #SMTP Integration 
 
-if DEBUG == "True":
+if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
